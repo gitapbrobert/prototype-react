63e189f9633afce8a52c97c0d1614cfc0939d465
gitapbrobert <robertoquintero@apbgroup.net>
2025-05-09 16:34:11 -0400

    added sidebar and header

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,32 @@
 import React from 'react';
-import { Gantt } from "wx-react-gantt";
+import { Gantt, WillowDark } from "wx-react-gantt";
 import { Willow } from "wx-react-gantt";
 import "wx-react-gantt/dist/gantt.css";
 import MyGanttComponent from './Gantt';
-
+import SideBar from './SideBar';
+import Header from './Header';
 
 
 function App() {
   return (
     <>
-    <Willow>
-      <MyGanttComponent/>
-    </Willow>
+    <div>
+        <Header></Header>
+      </div>
+    <div className="d-flex">
+      
+      
+      
+      
+      
+      <div className="flex-grow-1 p-3">
+        <WillowDark>
+          <MyGanttComponent/>
+        </WillowDark>
+      </div>
+    </div>
+
+    
     </>
     
   );
