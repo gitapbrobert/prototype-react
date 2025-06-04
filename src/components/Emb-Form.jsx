7300af67679c050f65387153fca7dfffe45d5b8a
gitapbrobert <robertoquintero@apbgroup.net>
2025-06-04 07:49:04 -0400

    Update Emb-Form component with expandable rows

--- src/components/Emb-Form.jsx
+++ src/components/Emb-Form.jsx
@@ -1,6 +1,5 @@
-    
 import React from 'react';
-import { Form, ButtonToolbar, Button, Input, IconButton , Modal, SelectPicker, DatePicker, Slider, Table } from 'rsuite';
+import { Form, ButtonToolbar, Button, Input, IconButton, Modal, SelectPicker, DatePicker, Slider, Table } from 'rsuite';
 const { Column, HeaderCell, Cell } = Table;
 import "../data/orders_forms.js";
 import { getEmb } from '../data/orders_forms.js';
@@ -9,13 +8,12 @@ import { MdOutlineExpandLess } from "react-icons/md";
 
 const Textarea = React.forwardRef((props, ref) => <Input {...props} as="textarea" ref={ref} />);
 
-
-
+const rowKey = 'id';
 
 const EmbForm = ({task, setTask, Types, onAction, IsOpen}) => {
   const [expandedRowKeys, setExpandedRowKeys] = React.useState([]);
 
-  const handleExpanded = (rowData, dataKey) => {
+  const handleExpanded = (rowData) => {
     let open = false;
     const nextExpandedRowKeys = [];
 
@@ -33,16 +31,11 @@ const EmbForm = ({task, setTask, Types, onAction, IsOpen}) => {
 
     setExpandedRowKeys(nextExpandedRowKeys);
   };
-  // const handleOpen = () => setOpen(true);
+
   const handleClose = () => setOpen(false);
   const [temp, settemp] = React.useState(open ? task : []);
   const exit = () => onAction("close-form", task);
-  
-  const handleSubmit = () => {
-    console.log(formValue);
-  };
 
-  // DATA //
   const data = getEmb();
 
   return (
@@ -51,37 +44,69 @@ const EmbForm = ({task, setTask, Types, onAction, IsOpen}) => {
         <Modal.Title>Embarque</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-
-
-        {/* Tabla */}
-        <Table autoHeight={true} flexgrow={1} data={data} hover={false} shouldUpdateScroll={false}
+        <Table
+          autoHeight={true}
+          data={data}
+          rowKey={rowKey}
           expandedRowKeys={expandedRowKeys}
-          onRowClick={data => {console.log(data);}}
-          renderRowExpanded={renderRowExpanded}
+          renderRowExpanded={rowData => {
+            return (
+              <div className="p-3">
+                <h6>Additional Details</h6>
+                <div className="row">
+                  <div className="col-md-6">
+                    <p><strong>Status:</strong> Active</p>
+                    <p><strong>Last Updated:</strong> {new Date().toLocaleDateString()}</p>
+                  </div>
+                  <div className="col-md-6">
+                    <p><strong>Notes:</strong> Sample additional information for {rowData.code}</p>
+                    <p><strong>Reference:</strong> REF-{rowData.id}</p>
+                  </div>
+                </div>
+              </div>
+            );
+          }}
         >
           <Column width={70} align="center">
             <HeaderCell>#</HeaderCell>
-            <ExpandCell dataKey="id" expandedRowKeys={expandedRowKeys} onChange={handleExpanded} />
+            <Cell>
+              {(rowData) => {
+                return (
+                  <IconButton
+                    size="sm"
+                    appearance="subtle"
+                    onClick={() => handleExpanded(rowData)}
+                    icon={
+                      expandedRowKeys.includes(rowData[rowKey]) ? 
+                        <MdOutlineExpandLess /> : 
+                        <MdOutlineExpandMore />
+                    }
+                  />
+                );
+              }}
+            </Cell>
           </Column>
+
           <Column flexGrow={1}>
             <HeaderCell>Codigo</HeaderCell>
-            <Cell dataKey="code" dataType="string" />
+            <Cell dataKey="code" />
           </Column>
+
           <Column flexGrow={1}>
             <HeaderCell>Modelo</HeaderCell>
-            <Cell dataKey="model" dataType="string" />
+            <Cell dataKey="model" />
           </Column>
+
           <Column flexGrow={1}>
             <HeaderCell>Cantidad (PT)</HeaderCell>
-            <Cell dataKey="amount_pf" dataType="number" />
+            <Cell dataKey="amount_pf" />
           </Column>
+
           <Column flexGrow={1}>
             <HeaderCell>Cantidad (PF)</HeaderCell>
-            <Cell dataKey="amount_emb" dataType="number" />
+            <Cell dataKey="amount_emb" />
           </Column>
         </Table>
-        {/* tabla */}
-
       </Modal.Body>
       <Modal.Footer>
         <Button onClick={exit} appearance="primary">
@@ -92,35 +117,6 @@ const EmbForm = ({task, setTask, Types, onAction, IsOpen}) => {
         </Button>
       </Modal.Footer>
     </Modal>
-
-  );
-};
-
-
-const ExpandCell = ({ rowData, dataKey, expandedRowKeys, onChange, ...props }) => (
-  <Cell {...props} style={{ padding: 5 }}>
-    <IconButton
-      appearance="subtle"
-      onClick={() => {
-        onChange(rowData);
-      }}
-      icon={
-        expandedRowKeys.some(key => key === rowData[rowKey]) ? (
-          <MdOutlineExpandLess />
-        ) : (
-          <MdOutlineExpandMore/>
-        )
-      }
-    />
-  </Cell>
-);
-
-const renderRowExpanded = rowData => {
-  return (
-    <div>
-      <p class="form-text text-muted">Help Text</p>
-      
-    </div>
   );
 };
 
--- src/data/orders_forms.js
+++ src/data/orders_forms.js
@@ -1,27 +1,39 @@
 export function getPF(){
     const data =[
         {
-            id: 1 ,
+            id: 1,
             code:'',
             model:'',
             amount_pt:0,
             amount_pf:0,
         },
-           
-    ]
-
+    ];
     return data;
 }
 
 export function getEmb(){
     const data = [
         {
-            id:1,
-            code:'codigo',
-            model:'modelo',
-            amount_pf:25,
-            amount_emb:21
+            id: 1,
+            code: 'CODE001',
+            model: 'Model A',
+            amount_pf: 25,
+            amount_emb: 21
         },
+        {
+            id: 2,
+            code: 'CODE001',
+            model: 'Model B',
+            amount_pf: 30,
+            amount_emb: 28
+        },
+        {
+            id: 3,
+            code: 'CODE002',
+            model: 'Model C',
+            amount_pf: 15,
+            amount_emb: 12
+        }
     ];
     return data;
 }
