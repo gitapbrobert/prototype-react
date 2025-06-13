e0c689665e3a0129f3c820d6a8158e44f82c18ca
gitapbrobert <robertoquintero@apbgroup.net>
2025-06-13 16:09:26 -0400

    vite ihy

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:'https://gitapbrobert.github.io/prototype-react',
   server: {
     host: true,
     port: 5173,
     strictPort: true,
+    
   }
+
 })
