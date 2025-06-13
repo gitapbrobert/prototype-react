fa3e43a5f4757fd858dd5063a3531363b4fc8143
gitapbrobert <robertoquintero@apbgroup.net>
2025-06-13 16:18:39 -0400

    this code should be better, srry

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,11 @@
 import React from 'react'; // Import React
 import ReactDOM from 'react-dom/client'; // Import ReactDOM
-import './assets/index.css';
 import App from './components/App.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    
+
     <App />
   </React.StrictMode>
 
